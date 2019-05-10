3e487864a073dc853c96e0ff7424f558dd8e81df
SevenRyuu <954585508@qq.com>
2019-05-10 17:18:29 +0800

    daily commit

--- source/seven-funny-front/src/pages/home/Home.vue
+++ source/seven-funny-front/src/pages/home/Home.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
+    <home-header></home-header>
     <h1>{{ msg }}</h1>
   </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 export default {
   name: 'Home',
+  components: {
+  	HomeHeader
+  },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
