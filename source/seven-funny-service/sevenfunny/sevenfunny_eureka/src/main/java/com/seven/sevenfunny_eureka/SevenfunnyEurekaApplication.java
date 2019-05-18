8e1f73bbf0f6efc742fb76dfb478865938198c98
Seven <954585508@qq.com>
2019-05-18 14:29:03 +0800

    init service env 、 add eureka

---
+++ source/seven-funny-service/sevenfunny/sevenfunny_eureka/src/main/java/com/seven/sevenfunny_eureka/SevenfunnyEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.seven.sevenfunny_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevenfunnyEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SevenfunnyEurekaApplication.class, args);
+    }
+
+}
