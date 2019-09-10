5db49dae7d19c06decafaa844cd046096611d37b
Seven <954585508@qq.com>
2019-09-10 18:41:04 +0800

    配置文件上传

---
+++ source/seven-funny-service/sevenfunny/sevenfunny-config/src/main/java/com/seven/config/SevenfunnyConfigApplication.java
@@ -0,0 +1,15 @@
+package com.seven.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SevenfunnyConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SevenfunnyConfigApplication.class, args);
+    }
+
+}
